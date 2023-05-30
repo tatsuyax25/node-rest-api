7e70f741b2c26dd1fca37be637aaa3287500bd27
Miguel Urena <urena_miguel82@outlook.com>
2023-05-30 13:54:20 -0700

    Send a response

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log("Server Listening on PORT:", port);
 });
+
+// response.send(status) is now a function that takes the JSON object as argument.
+app.get("/status", (request, response) => {
+  const status = {
+    "Status": "Running"
+  };
+  response.send(status);
+});
